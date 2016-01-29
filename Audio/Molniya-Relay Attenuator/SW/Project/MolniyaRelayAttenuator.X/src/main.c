8ca8e310d97a4fc912c73c2d4358707da296e35e
Ivan Vasilev <ivan.v.vasilev@gmail.com>
2016-01-29 22:16:38 +0200

    add mcc files for Zvezda board - will use it for development for now

--- Audio/Molniya-Relay Attenuator/SW/Project/MolniyaRelayAttenuator.X/src/main.c
+++ Audio/Molniya-Relay Attenuator/SW/Project/MolniyaRelayAttenuator.X/src/main.c
@@ -8,10 +8,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#include "mcc_generated_files/mcc.h"
+
+#include "cocoos/cocoos.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
+    
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
+    // Use the following macros to:
+
+    // Enable high priority global interrupts
+    //INTERRUPT_GlobalInterruptHighEnable();
+
+    // Enable low priority global interrupts.
+    //INTERRUPT_GlobalInterruptLowEnable();
+
+    // Disable high priority global interrupts
+    //INTERRUPT_GlobalInterruptHighDisable();
+
+    // Disable low priority global interrupts.
+    //INTERRUPT_GlobalInterruptLowDisable();
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    os_init();
+    
+    INTERRUPT_GlobalInterruptEnable();
+    INTERRUPT_PeripheralInterruptEnable();
+    
+    os_start();
 
     return (EXIT_SUCCESS);
 }
